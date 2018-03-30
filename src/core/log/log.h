41cc64256d32168d3e7a76cd661b969ffa2fb004
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2018-03-30 16:08:38 +0200

    Implémentation de macros pour tracer plus facilement le fonctionnement du programme.

---
+++ src/core/log/log.h
@@ -0,0 +1,18 @@
+#ifndef LOG_H
+#define LOG_H
+
+
+#define TRACE_DBG_BASE(argModule, argFormat,...)    \
+    fprintf(    stdout,                             \
+                "DBG: %s \t :: %s +%d (%s) \t=> "argFormat,     \
+                argModule, __FILE__, __LINE__, __FUNCTION__,   \
+                ##__VA_ARGS__ );
+
+#define TRACE_ERR_BASE(argModule, argFormat,...)    \
+    fprintf(    stderr,                             \
+                "ERR: %s \t :: %s +%d (%s) \t=> "argFormat,     \
+                argModule, __FILE__, __LINE__, __FUNCTION__,   \
+                ##__VA_ARGS__ );
+
+#endif  /*< LOG_H */
+
