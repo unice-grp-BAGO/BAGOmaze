4fa934ff2c550b9fae30974874f6d8e8af0f83ae
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2018-05-21 18:53:28 +0200

    Jeu aléatoire : Ajout d'un message lors de la sortie du labyrinthe.

--- src/ui/ui_play_random.c
+++ src/ui/ui_play_random.c
@@ -237,6 +237,17 @@ void    ui_play_random(TUiContext argContext)
         ui_play_movePlayer( argContext );
     }
 
+
+    char*   pp_text[3] = {
+        CDK_STYLE_TITLE2 "You won the game ! :-)",
+        "",
+        CDK_STYLE_EMPHASIS1 "<Press any key to continue>"
+    };
+
+    ui_popupBlocking( argContext->cdkscreen_p,
+                      pp_text, 3 );
+
+
     /* Release resources */
     grid_destroy( &(argContext->gridData) );
     clear();
